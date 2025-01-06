56df4956a1b39a118e97be16b7a2de7a18f46366
stepheeeen <stephenon56@gmail.com>
2025-01-07 00:45:16 +0100

    challenge-1

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -6,24 +6,30 @@ public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
-    public float verticalInput;
+    private float verticalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        // Get user input
+        forwardInput = Input.GetAxis("Vertical");
+        verticalInput = Input.GetAxis("Horizontal");
+
+    }
+
+    // FixedUpdate is called at a fixed time interval
     void FixedUpdate()
     {
-        // get the user's vertical input
-        verticalInput = Input.GetAxis("Vertical");
+        // Move the plane forward
+        transform.Translate(Vector3.forward * speed * forwardInput * Time.deltaTime);
 
-        // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
-
-        // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        // Tilt the plane up/down based on user input
+        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
     }
 }
